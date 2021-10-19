f6a5cc65d0fe809da21f4cb5f3ef78dc96551796
shaw_ds <30121988+shawds@users.noreply.github.com>
2021-10-19 08:13:44 -0400

    Add files via upload
    Katas7

---
+++ checkAir.js
@@ -0,0 +1,41 @@
+const checkAir = function (samples, threshold) {
+
+  var numClean = 0;
+  var numDirty = 0;
+  
+  for (var sample of samples) {
+    switch(sample) {
+      case 'clean':
+        numClean++;
+        break;
+      case 'dirty':
+        numDirty++;
+        break;
+    }
+  }
+  
+  var proportionDirty = numDirty / (numDirty + numClean);
+  
+  if (proportionDirty > threshold) {
+    return 'Polluted';
+  }
+  else
+    return 'Clean';
+}
+
+
+// Fn Call:
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
