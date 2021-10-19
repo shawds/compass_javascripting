fa44ba79db8b75a98f59cf5bf935178c8560d66c
shaw_ds <30121988+shawds@users.noreply.github.com>
2021-10-19 06:16:23 -0400

    Add files via upload
    Ex 4.11

---
+++ lastIndexOf.js
@@ -0,0 +1,16 @@
+function lastIndexOf(array, value){
+  for(var index = array.length; index > 0; index--){
+    if(array[index - 1] === value)
+      return index - 1;
+  }
+  return -1;
+}
+
+
+
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
