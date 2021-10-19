a5c317e9c096ea6959659c9cf3633b88f73f2f75
shaw_ds <30121988+shawds@users.noreply.github.com>
2021-10-19 06:57:28 -0400

    Add files via upload
    Katas3

---
+++ countVowels.js
@@ -0,0 +1,14 @@
+let numberOfVowels = function(data) 
+{
+  let count = 0;
+  for (var i = 0; i < data.length; i++) {
+    if(data[i] === 'a' || data[i] === 'e' || data[i] === 'i' || data[i] === 'o' || data[i] === 'u') {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
