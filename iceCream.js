cf3b6991abac02907562b1eb54e7be61a965a513
shaw_ds <30121988+shawds@users.noreply.github.com>
2021-10-19 05:30:24 -0400

    Add files via upload
    Ex 4.8

---
+++ iceCream.js
@@ -0,0 +1,9 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+
+iceCreamFlavours.push("root beer"); // Add a Flavor
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]); // Get the First Flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]); // Get the Last Flavour
+console.log(iceCreamFlavours.length); // Total #Flavours
